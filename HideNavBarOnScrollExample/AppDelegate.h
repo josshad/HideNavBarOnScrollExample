4c7f345d2338dd4b1f610de240f7ecccdbf9e772
Danila Gusev <danila.gusev@amaiz.com>
2017-12-06 16:05:07 +0300

    Initial Commit

---
+++ HideNavBarOnScrollExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HideNavBarOnScrollExample
+//
+//  Created by Danila Gusev on 12/6/17.
+//  Copyright © 2017 josshad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HideNavBarOnScrollExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HideNavBarOnScrollExample
+//
+//  Created by Danila Gusev on 12/6/17.
+//  Copyright © 2017 josshad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
