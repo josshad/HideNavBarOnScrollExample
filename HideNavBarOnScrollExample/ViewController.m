9f82be582b251f1cda92198913db22365e3f2d7e
Danila Gusev <danila.gusev@amaiz.com>
2017-12-06 16:41:08 +0300

    Code

--- HideNavBarOnScrollExample/ViewController.m
+++ HideNavBarOnScrollExample/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @end
 
@@ -16,14 +16,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage new]
+                                                  forBarMetrics:UIBarMetricsDefault];
+    self.navigationController.navigationBar.shadowImage = [UIImage new];
+    self.navigationController.navigationBar.translucent = YES;
+    self.navigationController.view.backgroundColor = [UIColor clearColor];
+    self.navigationController.navigationBar.backgroundColor = [UIColor clearColor];
+    self.navigationController.hidesBarsOnSwipe = YES;
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 50;
 }
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *const kCellIdentifier = @"kSettingsCell";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:kCellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:kCellIdentifier];
+    }
+    NSParameterAssert(indexPath.row >= 0);
+    cell.textLabel.text = [@(indexPath.row) stringValue];
+    return cell;
+}
 
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (scrollView.contentOffset.y < 0) {
+        self.navigationController.hidesBarsOnSwipe = NO;
+        [self.navigationController setNavigationBarHidden:NO animated: YES];
+    } else {
+        self.navigationController.hidesBarsOnSwipe = YES;
+    }
+}
 @end
